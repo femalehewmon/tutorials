306461b517ed086976744b20c02694cfc65a5ac4
femalehewmon <megan.van.welie@gmail.com>
2016-01-20 17:32:29 -0500

    remove should-have-been-ignored cmake files

--- opencv/LoadDisplay/CMakeFiles/LoadImage.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "LoadImage.pdb"
-  "LoadImage"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/LoadImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
